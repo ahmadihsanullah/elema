c845b102744a2656be0c3b106e534c44cc0ea0d5
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2024-12-18 10:07:53 +0700

    menambah model siswa

--- app/Models/Kelas.php
+++ app/Models/Kelas.php
@@ -4,19 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Kelas extends Model
 {
     protected $keyType = 'int';
     public $incrementing = true;
     public $timestamps = false;
-    
-   public function jurusan(): BelongsTo{
-       return $this->belongsTo(Jurusan::class, 'id_jurusan', 'id');
-   }
 
-   public function angkatan(): BelongsTo{
-    return $this->belongsTo(Angkatan::class, 'id_angkatan', 'id');
-}
+    public function jurusan(): BelongsTo
+    {
+        return $this->belongsTo(Jurusan::class, 'id_jurusan', 'id');
+    }
+
+    public function angkatan(): BelongsTo
+    {
+        return $this->belongsTo(Angkatan::class, 'id_angkatan', 'id');
+    }
+
+    public function siswas(): HasMany{
+        return $this->hasMany(Siswa::class, 'id_kelas', 'id');
+    }
 
 }
