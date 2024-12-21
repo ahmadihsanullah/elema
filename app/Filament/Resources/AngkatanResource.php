1e64fc3394e36f2dc7803ca470c13e760f4b64d0
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2024-12-21 21:01:34 +0700

    menambah resource angkatan, jurusan dan tahun pelajaran

---
+++ app/Filament/Resources/AngkatanResource.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AngkatanResource\Pages;
+use App\Filament\Resources\AngkatanResource\RelationManagers;
+use App\Models\Angkatan;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AngkatanResource extends Resource
+{
+    protected static ?string $model = Angkatan::class;
+    
+    protected static ?string $navigationIcon = 'heroicon-o-academic-cap';
+
+    protected static ?string $navigationGroup = 'Akademi';
+    
+    protected static ?string $navigationLabel = 'Angkatan';
+
+    public static function getModelLabel(): string
+    {
+        return 'Angkatan';
+    }
+    
+    public static function getPluralModelLabel(): string
+    {
+        return 'Angkatan';
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('tahun')
+                    ->required()
+                    ->unique(ignoreRecord: true),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('tahun'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAngkatans::route('/'),
+            'create' => Pages\CreateAngkatan::route('/create'),
+            'edit' => Pages\EditAngkatan::route('/{record}/edit'),
+        ];
+    }
+}
