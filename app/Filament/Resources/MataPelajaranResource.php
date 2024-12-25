866d7252278f6b302c90647793cd91fb9748ed36
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2024-12-25 21:48:56 +0700

    menambah resource mata pelajaran

---
+++ app/Filament/Resources/MataPelajaranResource.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MataPelajaranResource\Pages;
+use App\Filament\Resources\MataPelajaranResource\RelationManagers;
+use App\Models\MataPelajaran;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MataPelajaranResource extends Resource
+{
+    protected static ?string $model = MataPelajaran::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationGroup = 'Akademi';
+    
+    protected static ?string $navigationLabel = 'Mata Pelajaran';
+
+    protected static ?int $navigationSort = 1;
+
+    public static function getModelLabel(): string
+    {
+        return 'Mata Pelajaran';
+    }
+    
+    public static function getPluralModelLabel(): string
+    {
+        return 'Mata Pelajaran';
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nama')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('kode')
+                    ->required()
+                    ->maxLength(255)
+                    ->unique(ignoreRecord:true),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nama')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('kode')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMataPelajarans::route('/'),
+            'create' => Pages\CreateMataPelajaran::route('/create'),
+            'edit' => Pages\EditMataPelajaran::route('/{record}/edit'),
+        ];
+    }
+}
