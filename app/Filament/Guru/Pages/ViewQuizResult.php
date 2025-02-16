cfaf1c4304bff6f4626143d677efdcb88b03f9a1
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2025-02-16 15:07:52 +0700

    mengubah tampilan table pada hasil kuis dengan filament table

--- app/Filament/Guru/Pages/ViewQuizResult.php
+++ app/Filament/Guru/Pages/ViewQuizResult.php
@@ -5,9 +5,15 @@ namespace App\Filament\Guru\Pages;
 use App\Models\HasilKuis;
 use App\Models\Kuis;
 use Filament\Pages\Page;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 
-class ViewQuizResult extends Page
+class ViewQuizResult extends Page implements HasTable
 {
+    use InteractsWithTable;
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.guru.pages.view-quiz-result';
@@ -20,7 +26,7 @@ class ViewQuizResult extends Page
 
     public function mount($id)
     {
-        $this->kuis = Kuis::where('slug',$id)->first();
+        $this->kuis = Kuis::where('slug', $id)->first();
 
         if ($this->kuis) {
             // Retrieve quiz results for all students who have taken the quiz
@@ -28,6 +34,42 @@ class ViewQuizResult extends Page
                 ->where('id_kuis', $this->kuis->id)
                 ->get();
         }
+    }
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(fn() => HasilKuis::where('id_kuis', $this->kuis->id))
+            ->columns([
+                TextColumn::make('siswa.name')
+                    ->label('Nama Siswa'),
+                TextColumn::make('siswa.kelas.nama')
+                    ->label('Kelas'),
+                TextColumn::make('skor')
+                    ->label('Nilai')
+                    ->badge()
+            ])
+            ->filters([
+                // Filter untuk mencari siswa berdasarkan nama
+                \Filament\Tables\Filters\Filter::make('siswa_name')
+                    ->label('Cari Nama Siswa')
+                    ->form([
+                        \Filament\Forms\Components\TextInput::make('siswa_name')
+                            ->label('Nama Siswa')
+                            ->placeholder('Cari nama siswa...')
+                    ])
+                    ->query(fn(Builder $query, array $data) => $query->when(
+                        $data['siswa_name'],
+                        fn(Builder $query, $name) =>
+                        $query->whereHas('siswa', fn(Builder $query) => $query->where('name', 'like', "%{$name}%"))
+                    )),
+            ]);
+        ;
+    }
+
+    public function backToSession()
+    {
+        return redirect()->route('filament.guru.resources.sesi-belajars.index');
+
     }
 
     public function getResults()
--- resources/views/filament/guru/pages/view-quiz-result.blade.php
+++ resources/views/filament/guru/pages/view-quiz-result.blade.php
@@ -1,31 +1,14 @@
 <!-- filepath: resources/views/filament/guru/pages/view-quiz-result.blade.php -->
 <x-filament::page>
+    <div>
+        <x-filament::link wire:click="backToSession" tag="button">
+            Kembali
+        </x-filament::link>
+    </div>
     <x-filament::card>
-        
-        <p class="text-2xl font-bold mb-4">Daftar Siswa yang menyelesaikan kuis:  {{ $kuis->judul }}</p>
-        <p class="text-gray-500 mb-4">Created at: {{ $kuis->created_at }}</p>
-        
+        <p class="text-xl text-black-700 mb-4">Daftar Siswa yang menyelesaikan kuis:  {{ $kuis->judul }}</p>
         <div class="overflow-x-auto">
-            <table class="w-full bg-white border border-gray-200">
-                <thead>
-                    <tr class="bg-gray-100 border-b">
-                        <th class="text-left py-3 px-6 border-r">No</th>
-                        <th class="text-left py-3 px-6 border-r">Nama</th>
-                        <th class="text-left py-3 px-6 border-r">Kelas</th>
-                        <th class="text-left py-3 px-6">Score</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($hasilKuis as $hk)
-                        <tr class="border-b hover:bg-gray-50">
-                            <td class="py-3 px-6 border-r">{{ $loop->iteration }}</td>
-                            <td class="py-3 px-6 border-r">{{ $hk->siswa->name }}</td>
-                            <td class="py-3 px-6 border-r">{{ $hk->siswa->kelas->nama }}</td>
-                            <td class="py-3 px-6">{{ $hk->skor }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            {{$this->table}}
         </div>
     </x-filament::card>
 </x-filament::page>
