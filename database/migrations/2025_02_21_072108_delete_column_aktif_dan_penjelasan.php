23b8a9aabb7273bb095e4b666fefb50be2bea395
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2025-05-07 20:34:05 +0700

    fixx error:  migration pertanyaan

--- database/migrations/2025_02_21_072108_delete_column_aktif_dan_penjelasan.php
+++ database/migrations/2025_02_21_072108_delete_column_aktif_dan_penjelasan.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::table('pertanyaans', function (Blueprint $table) {
             $table->dropColumn('penjelasan');
-            $table->dropColumn('aktif');
         });
     }
 
@@ -24,7 +23,6 @@ return new class extends Migration
     {
         Schema::table('pertanyaans', function (Blueprint $table) {
             $table->text('penjelasan')->nullable(true);
-            $table->boolean('aktif')->default(true);
         });
     }
 };
